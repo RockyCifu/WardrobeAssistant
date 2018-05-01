9c41d1a1e758ce0f6746d05322e33a1e0326f1de
MamathaK <mamathak50@gmail.com>
2018-04-30 19:24:01 -0700

    Initial commit

---
+++ app/src/main/java/lwtech/itad230/it_wa_databaseconnection/Constants.java
@@ -0,0 +1,10 @@
+package lwtech.itad230.it_wa_databaseconnection;
+
+public class Constants {
+    private static final String ROOT_URL = "http://10.0.0.154:8181/IT_project/web_services/";
+    public static final String URL_REGISTER = ROOT_URL+"register_user.php";
+    public static final String URL_LOGIN = ROOT_URL+"user_login.php";
+    public static final String URL_ADDTAG = ROOT_URL+"add_tags.php";
+
+}
+
---
+++ app/src/main/java/lwtech/itad230/it_wa_databaseconnection/SharedPrefManager.java
@@ -0,0 +1,56 @@
+package lwtech.itad230.it_wa_databaseconnection;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.android.volley.toolbox.StringRequest;
+
+public class SharedPrefManager {
+    private static SharedPrefManager mInstance;
+    private static Context mCtx;
+
+    private static final String SHARED_PREF_NAME = "mysharedpref12";
+    private static final String KEY_USERNAME = "user_name";
+    private static final String KEY_USERID = "user_id";
+    private SharedPrefManager(Context context) {
+        mCtx = context;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(context);
+        }
+        return mInstance;
+    }
+
+    public boolean userlogin(int user_id, String user_name)
+    {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(KEY_USERID, user_id);
+        editor.putString(KEY_USERNAME, user_name);
+        editor.apply();
+        return true;
+    }
+
+    public boolean isLoggedIn()
+    {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        if(sharedPreferences.getString(KEY_USERNAME,null)!= null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean logOut()
+    {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        return true;
+
+
+    }
+}
